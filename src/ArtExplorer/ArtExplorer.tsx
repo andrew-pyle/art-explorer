4e03d7681ebdda2b95a595c22b8e54d4d20dff6b
Andrew Pyle <andrew.pyle@icloud.com>
2023-05-13 14:35:14 -0500

    format with rome

--- src/ArtExplorer/ArtExplorer.tsx
+++ src/ArtExplorer/ArtExplorer.tsx
@@ -16,137 +16,137 @@ const metMuseumCollection = new MetMuseumCollection();
 type Props = {};
 
 interface State {
-  userNotice: string;
-  loading: boolean;
-  artworks: ArtworkT[];
+	userNotice: string;
+	loading: boolean;
+	artworks: ArtworkT[];
 }
 
 // Component
 
 export class ArtExplorer extends Component<Props, State> {
-  state: State = {
-    userNotice: "",
-    loading: false,
-    artworks: [],
-  };
+	state: State = {
+		userNotice: "",
+		loading: false,
+		artworks: [],
+	};
 
-  searchMetMuseumApi: ArtworkSearchHandler = async (query, options) => {
-    try {
-      // Set Loading
-      this.setState({ userNotice: "Loading...", loading: true });
+	searchMetMuseumApi: ArtworkSearchHandler = async (query, options) => {
+		try {
+			// Set Loading
+			this.setState({ userNotice: "Loading...", loading: true });
 
-      // Do Search
-      const resultListPromises = await metMuseumCollection.explore(
-        query,
-        options
-      );
+			// Do Search
+			const resultListPromises = await metMuseumCollection.explore(
+				query,
+				options,
+			);
 
-      // TODO. Use Set()
-      // Remove artworks which aren't in the new results set
-      this.setState((prevState) => {
-        const newArtworks = new Set(resultListPromises.map((p) => p.id));
-        const stillMatchingArtworks = prevState.artworks.filter((old) =>
-          newArtworks.has(old.id)
-        );
-        return {
-          artworks: stillMatchingArtworks,
-        };
-      });
+			// TODO. Use Set()
+			// Remove artworks which aren't in the new results set
+			this.setState((prevState) => {
+				const newArtworks = new Set(resultListPromises.map((p) => p.id));
+				const stillMatchingArtworks = prevState.artworks.filter((old) =>
+					newArtworks.has(old.id),
+				);
+				return {
+					artworks: stillMatchingArtworks,
+				};
+			});
 
-      // Update the state asynchronously as each Request settles.
-      // Each Request represents an artwork and is independent of the others.
-      // Non-awaiting code. Always moves on.
-      for (const { artwork } of resultListPromises) {
-        artwork
-          .then((response) => this.updateResults(response))
-          // Let any one Request fail silently.
-          .catch((err) => console.error(err));
-      }
+			// Update the state asynchronously as each Request settles.
+			// Each Request represents an artwork and is independent of the others.
+			// Non-awaiting code. Always moves on.
+			for (const { artwork } of resultListPromises) {
+				artwork
+					.then((response) => this.updateResults(response))
+					// Let any one Request fail silently.
+					.catch((err) => console.error(err));
+			}
 
-      // Also await the entire Request set to end
-      // Loading only after all Requests have settled.
-      // Awaiting code. Stops here until all requests have settled.
-      const artworks = await Promise.allSettled(
-        resultListPromises.map((r) => r.artwork)
-      );
+			// Also await the entire Request set to end
+			// Loading only after all Requests have settled.
+			// Awaiting code. Stops here until all requests have settled.
+			const artworks = await Promise.allSettled(
+				resultListPromises.map((r) => r.artwork),
+			);
 
-      // No Results Error View
-      if (artworks.length === 0) {
-        this.setState({
-          loading: false,
-          userNotice: "😭 No results in the Met Museum Open Access Collection.",
-        });
-        return;
-      }
+			// No Results Error View
+			if (artworks.length === 0) {
+				this.setState({
+					loading: false,
+					userNotice: "😭 No results in the Met Museum Open Access Collection.",
+				});
+				return;
+			}
 
-      // Success View
-      this.setState({
-        loading: false,
-        userNotice: "", // clear loading notice
-      });
-    } catch (err) {
-      // Unknown Error View
-      console.error(err);
-      // User Notice
-      this.setState({
-        loading: false,
-        userNotice: "😰 We are having trouble at the moment. Sorry!",
-      });
-    }
-  };
+			// Success View
+			this.setState({
+				loading: false,
+				userNotice: "", // clear loading notice
+			});
+		} catch (err) {
+			// Unknown Error View
+			console.error(err);
+			// User Notice
+			this.setState({
+				loading: false,
+				userNotice: "😰 We are having trouble at the moment. Sorry!",
+			});
+		}
+	};
 
-  updateResults = async (response: MetCollectionObjectResponse) => {
-    try {
-      const artwork = parseArtwork(response);
-      if (!artwork.imgSrc) {
-        // Some results from the MET API do not have an image.
-        // Skip these silently.
-        throw new Error(
-          `Skipping Result because it has no image src: ${JSON.stringify(
-            artwork
-          )}`
-        );
-      }
+	updateResults = async (response: MetCollectionObjectResponse) => {
+		try {
+			const artwork = parseArtwork(response);
+			if (!artwork.imgSrc) {
+				// Some results from the MET API do not have an image.
+				// Skip these silently.
+				throw new Error(
+					`Skipping Result because it has no image src: ${JSON.stringify(
+						artwork,
+					)}`,
+				);
+			}
 
-      // Append the artwork to the results set
-      this.setState((prevState) => {
-        const prevArtworks = new Set(prevState.artworks);
-        const newArtworks = prevArtworks.add(artwork); // prevent duplication
-        const sortedArtworks = Array.from(newArtworks).sort(
-          (a, b) => (a.year = b.year)
-        );
-        return {
-          artworks: sortedArtworks,
-        };
-      });
-    } catch (err) {
-      console.log(err);
-      // Let any one result fail silently.
-    }
-  };
+			// Append the artwork to the results set
+			this.setState((prevState) => {
+				const prevArtworks = new Set(prevState.artworks);
+				const newArtworks = prevArtworks.add(artwork); // prevent duplication
+				const sortedArtworks = Array.from(newArtworks).sort(
+					(a, b) => a.year - b.year,
+				);
+				return {
+					artworks: sortedArtworks,
+				};
+			});
+		} catch (err) {
+			console.log(err);
+			// Let any one result fail silently.
+		}
+	};
 
-  render() {
-    return (
-      <Fragment>
-        <h1>Art Timeline</h1>
-        <p>
-          Explore Representations in Art through Time With the Met Museum of
-          Art's Open Access Collection
-        </p>
-        <h2>Search</h2>
-        <div class={style["sticky-top-bar"]}>
-          <ArtSearch onSubmit={this.searchMetMuseumApi} />
-          <hr />
-        </div>
-        <div class={style["title-bar"]}>
-          <h2>Results</h2>
-          {this.state.userNotice ? (
-            <p class={style["user-notice"]}>{this.state.userNotice}</p>
-          ) : null}
-          {this.state.loading ? <loading-indicator /> : null}
-        </div>
-        <ArtResultsGrid results={this.state.artworks} />
-      </Fragment>
-    );
-  }
+	render() {
+		return (
+			<Fragment>
+				<h1>Art Timeline</h1>
+				<p>
+					Explore Representations in Art through Time With the Met Museum of
+					Art's Open Access Collection
+				</p>
+				<h2>Search</h2>
+				<div class={style["sticky-top-bar"]}>
+					<ArtSearch onSubmit={this.searchMetMuseumApi} />
+					<hr />
+				</div>
+				<div class={style["title-bar"]}>
+					<h2>Results</h2>
+					{this.state.userNotice ? (
+						<p class={style["user-notice"]}>{this.state.userNotice}</p>
+					) : null}
+					{this.state.loading ? <loading-indicator /> : null}
+				</div>
+				<ArtResultsGrid results={this.state.artworks} />
+			</Fragment>
+		);
+	}
 }
--- src/ArtResultsGrid/ArtResultsGrid.tsx
+++ src/ArtResultsGrid/ArtResultsGrid.tsx
@@ -8,25 +8,25 @@ import "./transition.css";
 // Types
 
 interface Props {
-  results: ArtworkT[];
+	results: ArtworkT[];
 }
 
 // Component
 
 export function ArtResultsGrid({ results }: Props) {
-  // 4 x 3 Cell Blocks
-  const cellWidth = 200;
-  const cellHeight = 200;
+	// 4 x 3 Cell Blocks
+	const cellWidth = 200;
+	const cellHeight = 200;
 
-  return (
-    <TransitionGroup component="ul" class={style["result-grid"]}>
-      {results.map((art) => (
-        <CSSTransition key={art.id} class="fade" timeout={500}>
-          <li>
-            <Artwork width={cellWidth} height={cellHeight} {...art} />
-          </li>
-        </CSSTransition>
-      ))}
-    </TransitionGroup>
-  );
+	return (
+		<TransitionGroup component="ul" class={style["result-grid"]}>
+			{results.map((art) => (
+				<CSSTransition key={art.id} class="fade" timeout={500}>
+					<li>
+						<Artwork width={cellWidth} height={cellHeight} {...art} />
+					</li>
+				</CSSTransition>
+			))}
+		</TransitionGroup>
+	);
 }
